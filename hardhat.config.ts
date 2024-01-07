666c7ecd8b6b2045e278268ed1f34236b33a7cf4
Mateo Del Rosario Chauchet <mateo.chauchet@mercadolibre.com>
2024-01-07 17:09:56 -0300

    finish hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
 
-  networks: {},
+  networks: {
+    sepolia: {
+      url: process.env.ETHEREUM_SEPOLIA_TESTNET_RPC,
+      accounts: [process.env.PRIVATE_KEY || '']
+    }
+  },
 
-  etherscan: {},
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
 
   paths: {
     artifacts: './artifacts',
